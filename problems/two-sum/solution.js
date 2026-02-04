993ca4fa29de41ca3c7fc267e031287346f18157
Adrian Dobkowski <156909247+adriandobkowski@users.noreply.github.com>
2026-02-04 12:23:48 +0100

    Solve two-sum

---
+++ problems/two-sum/solution.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const numMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const complement = target - currentNum;
+
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+
+        numMap.set(currentNum, i);
+    }
+
+    return [];
+}
